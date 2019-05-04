f5339400031ab7469f22dcf39d3273746158e677
Luc Nguyen <luc.nguyen@ut.edu.vn>
2019-05-04 16:00:28 +0700

    Update 2

--- src/App.js
+++ src/App.js
@@ -1,52 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      token: ''
+    };
+    this.updateToken = this.updateToken.bind(this);
+    this.updateUrl = this.updateUrl.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  updateToken(event) {
+    this.setState({ token: event.target.value })
+  }
+  updateUrl(event) {
+    this.setState({ url: event.target.value })
+  }
+  handleClick() {
+    axios.patch(''+this.state.url, {
 
-        this.state = {
-            data: null,
-        };
-    }
+    }, {
+        headers: {
+          Authorization: 'Bearer ' + this.state.token
+        }
+      })
+      .then(function (response) {
+        console.log("TCL: App -> componentDidMount -> response", response);
+      });
 
-    componentDidMount() {
-        axios.patch('http://api2.abc.com/v2/tests/8', {
+  }
 
-        },{
-            headers: {
-                Authorization: 'Bearer 40cb3cf32b20e96610afd03a43910035ad859e8d5a77df7e0f867ae046a09a7b/0jWakLR1hKDDetKoruRhYuXpEqCnOmv02SgtlDPLXUPQ2-dXfEvbC6bEcIArOoju/MTA='
-            }
-        })
-            .then(function (response) {
-								console.log("TCL: App -> componentDidMount -> response", response);
-            });
-    }
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        );
-    }
+
+
+  render() {
+    return (
+      <div className="App">
+
+
+
+        <label>token</label>
+        <input id="token" type="text" name="token" onChange={this.updateToken} />
+        <br />
+        <label>url</label>
+        <input id="url" type="text" name="url" onChange={this.updateUrl} />
+        <button id="btn" name="btn" onClick={this.handleClick}>Submit</button>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
